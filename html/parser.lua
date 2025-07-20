2be7d0d5e984044be03ffa3b7572c78ff542bc36
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2025-07-20 01:40:09 +0100

    FInally finish parser

--- html/parser.lua
+++ html/parser.lua
@@ -1,21 +1,7 @@
--- void elements are self closing tags (implied close even if close tag not present)
--- https://developer.mozilla.org/en-US/docs/Glossary/Void_element
-local voidElements = {
-    area = true,
-    base = true,
-    br = true,
-    col = true,
-    embed = true,
-    hr = true,
-    img = true,
-    input = true,
-    link = true,
-    meta = true,
-    param = true,
-    source = true,
-    track = true,
-    wbr = true
-}
+local voidElements = require("./voidElements")
+
+local insert = table.insert
+local remove = table.remove
 
 local function parse(tokens)
     local root = { type = "root", children = {} }
@@ -29,11 +15,11 @@ local function parse(tokens)
                 props = token.props,
                 children = {}
             }
-            table.insert(stack[#stack].children, node)
+            insert(stack[#stack].children, node)
 
             -- the fix for void elements was somehow actually easy
             if not voidElements[token.name] then
-                table.insert(stack, node)
+                insert(stack, node)
             end
         elseif token.type == "tag_self" then
             local node = {
@@ -42,15 +28,16 @@ local function parse(tokens)
                 props = token.props,
                 children = {}
             }
-            table.insert(stack[#stack].children, node)
-        elseif token.type == "text" then
-            table.insert(stack[#stack].children, {
-                type = "text",
+            insert(stack[#stack].children, node)
+        elseif token.type == "text" or token.type == "directive" then
+            -- insert(stack[#stack].children, token)
+            insert(stack[#stack].children, {
+                type = token.type,
                 value = token.value
             })
         elseif token.type == "tag_close" then
             if stack[#stack].tag == token.name then
-                table.remove(stack)
+                remove(stack)
             else
                 print("mismatched closing tag: " .. token.name)
                 -- optional error handling probably later idk
