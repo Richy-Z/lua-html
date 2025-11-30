49cdaca06d1abefa2ea73706a090abace0aee65f
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2025-11-30 04:11:50 +0000

    Init

--- src/init.lua
+++ src/init.lua
@@ -1,3 +1,6 @@
-local html = {}
-
-return html
+return {
+    tokenise = require("./tokeniser"),
+    parse = require("./parser"),
+    sanitise = require("./sanitiser"),
+    serialise = require("./serialise")
+}
